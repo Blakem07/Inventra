7cc180ab8f8990552a5996766835d6038c0e216b
Blakem07 <Blakem2020@gmail.com>
2026-02-14 14:14:19 +0000

    feat(category): add POST /categories to create a new category document

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -19,4 +19,23 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+/**
+ * POST /categories
+ *
+ * Creates a new category.
+ *
+ * Returns the newly created category document.
+ */
+router.post("/", async (req, res, next) => {
+  try {
+    const { name } = req.body;
+
+    const newCategory = await Category.create({ name });
+
+    res.status(201).json(newCategory);
+  } catch (err) {
+    next(err);
+  }
+});
+
 export default router;
