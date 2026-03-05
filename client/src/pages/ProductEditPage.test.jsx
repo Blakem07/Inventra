8a33484018b0254de9acb5e9741b385412fc9370
Blakem07 <Blakem2020@gmail.com>
2026-03-05 12:47:47 +0000

    test(ProductEditPage): renders product update input fields (RED)

--- client/src/pages/ProductEditPage.test.jsx
+++ client/src/pages/ProductEditPage.test.jsx
@@ -1,9 +1,19 @@
-import { describe, it, expect, beforeEach } from "vitest";
+import { describe, it, expect, beforeEach, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { createMemoryRouter, RouterProvider, MemoryRouter, useLocation } from "react-router-dom";
 import { routes } from "../app/routes";
 
+import { testProducts } from "../tests/testProducts";
+
 describe("Product Edit Page Tests", () => {
+  let products;
+
+  beforeEach(() => {
+    products = testProducts.map((product) => ({ ...product })); // For mutation in archive test
+
+    global.fetch = vi.fn();
+  });
+
   it("loads route param and shows edit mode with id", async () => {
     const router = createMemoryRouter(routes, { initialEntries: ["/inventory/123/edit"] });
     render(<RouterProvider router={router} />);
@@ -11,5 +21,26 @@ describe("Product Edit Page Tests", () => {
     expect(screen.getByTestId("product-edit-page")).toBeInTheDocument();
     expect(screen.getByRole("heading", { name: /edit page/i }));
     expect(screen.getByRole("heading", { name: /id:\s*123/i })).toBeInTheDocument();
+  });
+
+  it("renders product update input fields", async () => {
+    const router = createMemoryRouter(routes, { initialEntries: ["/inventory/123/edit"] });
+    render(<RouterProvider router={router} />);
+
+    expect(screen.getByTestId("product-edit-page")).toBeInTheDocument();
+
+    // Wait for fetch to load categories
+    const dropbox = screen.getByRole("combobox");
+    expect(
+      await within(dropbox).findByRole("option", { name: categories[0].name }),
+    ).toBeInTheDocument();
+
+    expect(screen.getByLabelText("Name")).toBeInTheDocument();
+    expect(screen.getByLabelText("Category")).toBeInTheDocument();
+    expect(screen.getByLabelText("SKU or Barcode")).toBeInTheDocument();
+    expect(screen.getByLabelText("Price")).toBeInTheDocument();
+    expect(screen.getByLabelText("Reorder Level")).toBeInTheDocument();
+
+    expect(screen.getByRole("button", { name: /save/i })).toBeInTheDocument();
   });
 });
