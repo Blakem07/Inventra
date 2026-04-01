6e5b60ebea3f55529e45bd4b750b1d9e502e33e2
Blakem07 <Blakem2020@gmail.com>
2026-04-01 16:16:36 +0100

    feat(backend): demoAccess should accept correct password (GREEN)

--- server/routes/demoRoutes.js
+++ server/routes/demoRoutes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 
+import crypto from "crypto";
+
 const router = Router();
 
 router.post("/access", async (req, res, next) => {
@@ -9,6 +11,15 @@ router.post("/access", async (req, res, next) => {
     if (password !== process.env.DEMO_PASSWORD) {
       return res.status(401).json({ message: "Invalid password" });
     }
+
+    const message = "demo access";
+    const hmac = crypto.createHmac("sha256", process.env.DEMO_COOKIE_SECRET);
+    const signature = hmac.update(message).digest("hex");
+    const token = `${message}.${signature}`;
+    res.cookie("demo_auth", token, {
+      httpOnly: true,
+      sameSite: "lax",
+    });
 
     // success case will be implemented later
     return res.status(200).json({ success: true });
