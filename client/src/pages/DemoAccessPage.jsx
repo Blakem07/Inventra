48c5095591f2b39d7ea1dad5764b1d64a438b44f
Blakem07 <Blakem2020@gmail.com>
2026-04-03 20:58:35 +0100

    feat(frontend, demoAccessPage): add html skeleton (GREEN)

--- client/src/pages/DemoAccessPage.jsx
+++ client/src/pages/DemoAccessPage.jsx
@@ -1,7 +1,25 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+
 export default function DemoAccessPage() {
   return (
-    <div data-testid="demo-access-page">
-      <h1>Demo Access</h1>
+    <div data-testid="demo-access-page" className="h-screen flex items-center justify-center ">
+      <Card className="w-full max-w-md p-5">
+        <CardHeader>
+          <CardTitle className="text-xl">Inventra Demo Access</CardTitle>
+        </CardHeader>
+
+        <CardContent className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" />
+          </div>
+
+          <Button className="w-full">Enter</Button>
+        </CardContent>
+      </Card>
     </div>
   );
 }
